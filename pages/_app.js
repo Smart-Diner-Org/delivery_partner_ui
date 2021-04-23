290c45739fa2eccadfd702576306e9849eddc7b7
vinayveerappaji <vinayveerappaji@gmail.com>
2021-04-23 22:27:51 +0530

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import 'antd/dist/antd.css';
+import "../styles/globals.css";
+import "antd/dist/antd.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/fav.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
